680b5fcb53662f508bd3eb3545d869a541474b27
Juan <juanpereiracosta@gmail.com>
2024-09-09 00:13:06 +0100

    Finalização

--- app/src/main/java/com/example/app01/MainActivity.kt
+++ app/src/main/java/com/example/app01/MainActivity.kt
@@ -1,20 +1,22 @@
 package com.example.app01
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.app01.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        binding.buttonOla.setOnClickListener {
+            var nome: String = binding.editNome.text.toString()
+            binding.textResultado.text = "Olá, $nome"
         }
     }
 }
